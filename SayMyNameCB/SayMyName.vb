047eafd36d1fdc8aae9af5e1560ffdf833e2c924
CL Bogart <clbogart3@outlook.com>
2022-01-19 19:10:50 -0700

    Add project files.

---
+++ SayMyNameCB/SayMyName.vb
@@ -0,0 +1,22 @@
+﻿Option Explicit On
+Option Strict On
+Module SayMyName
+    'Carson Bogart
+    'RCET0265  
+    'Spring 2022
+    'Say My Name
+    '
+    Sub Main()
+        'set userInput as string input
+        Dim userInput As String
+        'display text
+        Console.WriteLine("What's your name?")
+        'wait for user input
+        userInput = Console.ReadLine
+        'read user input 
+        Console.WriteLine("Hello " & userInput & " I am the computer, nice to meet you!")
+        'wait for user
+        Console.ReadLine()
+    End Sub
+
+End Module
